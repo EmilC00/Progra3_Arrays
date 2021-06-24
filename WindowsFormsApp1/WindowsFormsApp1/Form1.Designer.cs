cc773c85f4c9fa6cc15a280060678f80426641e8
EmilC00 <86421650+EmilC00@users.noreply.github.com>
2021-06-24 10:20:06 -0600

    2do array
    Arreglo segunda clase

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class orden_btn
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Semana_listbox = new System.Windows.Forms.ListBox();
+            this.dias_listBox = new System.Windows.Forms.ListBox();
+            this.ejecutara_btn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ordenarlistbox = new System.Windows.Forms.ListBox();
+            this.max_min_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Semana_listbox
+            // 
+            this.Semana_listbox.FormattingEnabled = true;
+            this.Semana_listbox.Location = new System.Drawing.Point(297, 118);
+            this.Semana_listbox.Name = "Semana_listbox";
+            this.Semana_listbox.Size = new System.Drawing.Size(157, 251);
+            this.Semana_listbox.TabIndex = 8;
+            // 
+            // dias_listBox
+            // 
+            this.dias_listBox.FormattingEnabled = true;
+            this.dias_listBox.Location = new System.Drawing.Point(94, 118);
+            this.dias_listBox.Name = "dias_listBox";
+            this.dias_listBox.Size = new System.Drawing.Size(148, 251);
+            this.dias_listBox.TabIndex = 7;
+            // 
+            // ejecutara_btn
+            // 
+            this.ejecutara_btn.Location = new System.Drawing.Point(79, 27);
+            this.ejecutara_btn.Name = "ejecutara_btn";
+            this.ejecutara_btn.Size = new System.Drawing.Size(173, 48);
+            this.ejecutara_btn.TabIndex = 6;
+            this.ejecutara_btn.Text = "Ejecutar";
+            this.ejecutara_btn.UseVisualStyleBackColor = true;
+            this.ejecutara_btn.Click += new System.EventHandler(this.ejecutara_btn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 48);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Ordenar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ordenarlistbox
+            // 
+            this.ordenarlistbox.FormattingEnabled = true;
+            this.ordenarlistbox.Location = new System.Drawing.Point(513, 118);
+            this.ordenarlistbox.Name = "ordenarlistbox";
+            this.ordenarlistbox.Size = new System.Drawing.Size(157, 251);
+            this.ordenarlistbox.TabIndex = 10;
+            // 
+            // max_min_btn
+            // 
+            this.max_min_btn.Location = new System.Drawing.Point(615, 27);
+            this.max_min_btn.Name = "max_min_btn";
+            this.max_min_btn.Size = new System.Drawing.Size(173, 48);
+            this.max_min_btn.TabIndex = 11;
+            this.max_min_btn.Text = "Max-Min";
+            this.max_min_btn.UseVisualStyleBackColor = true;
+            this.max_min_btn.Click += new System.EventHandler(this.max_min_btn_Click);
+            // 
+            // orden_btn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.max_min_btn);
+            this.Controls.Add(this.ordenarlistbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Semana_listbox);
+            this.Controls.Add(this.dias_listBox);
+            this.Controls.Add(this.ejecutara_btn);
+            this.Name = "orden_btn";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Semana_listbox;
+        private System.Windows.Forms.ListBox dias_listBox;
+        private System.Windows.Forms.Button ejecutara_btn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox ordenarlistbox;
+        private System.Windows.Forms.Button max_min_btn;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class orden_btn : Form
+    {
+        public orden_btn()
+        {
+            InitializeComponent();
+        }
+
+        private void ejecutara_btn_Click(object sender, EventArgs e)
+        {
+            //Arreglo
+            int[] arra1 = new int[5];
+
+            arra1[0] = 10;
+            arra1[1] = 20;
+            arra1[2] = 30;
+            arra1[3] = 40;
+            arra1[4] = 50;
+
+            int[] arra2 = new int[] { 1, 2, 3, 4, 5 };
+            //---------------------------------
+            int[] arra3 = { 1, 2, 3, 4, 5 };
+
+            string[] dias_semana = new string[] { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo" };
+
+            for (int i = 0; i < arra1.Length; i++)
+            {
+                dias_listBox.Items.Add(arra1[i]);
+            }
+
+            foreach (var y in dias_semana)
+            {
+                Semana_listbox.Items.Add(y);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] valores = new int[] { 3, 5, 1, 7, 8, 10, 2, 4 };
+
+            //Ordenar de menor a mayor 
+            Array.Sort(valores);
+
+            //Ordenar de menor a mayor 
+            Array.Reverse(valores);
+            foreach (var item in valores)
+            {
+                ordenarlistbox.Items.Add(item);
+            }
+
+
+        }
+
+        private void max_min_btn_Click(object sender, EventArgs e)
+        {
+            int[] valores = new int[] { 3, 5, 1, 7, 8, 10, 2, 4 };
+
+            int ValMax = valores[0];
+            int valmin = valores[0];
+
+            for (int i = 1; i < valores.Length; i++)
+            {
+                if (valores[i]>ValMax)
+                {
+                    ValMax = valores[i];
+
+                }
+
+                if (valores[i] < valmin)
+                {
+                    valmin = valores[i];
+
+                }
+            }
+
+            MessageBox.Show("Valor máximo:  " + ValMax + "/ Valor mínimo:  " + valmin);
+                
+        }
+    }
+}
